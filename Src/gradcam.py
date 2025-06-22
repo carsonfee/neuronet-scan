bc621a176814d89f44aa7ffe769ae70eea14272d
Carson Fee <carseomighty@gmail.com>
2025-06-22 00:32:15 -0500

    Create gradcam.py

---
+++ Src/gradcam.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array)
+        class_idx = tf.argmax(predictions[0])
+        loss = predictions[:, class_idx]
+
+    grads = tape.gradient(loss, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    conv_outputs = conv_outputs[0]
+
+    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+def overlay_heatmap(original_img, heatmap, alpha=0.4):
+    heatmap = cv2.resize(heatmap, original_img.size)
+    heatmap = np.uint8(255 * heatmap)
+    heatmap_color = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    overlayed = cv2.addWeighted(np.array(original_img), 1 - alpha, heatmap_color, alpha, 0)
+    return overlayed
+  
